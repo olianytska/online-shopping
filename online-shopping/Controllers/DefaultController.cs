49da90d9aa1dbe7d09ef204655e1dc732997e223
Liza Olianyts'ka <63195522+olianytska@users.noreply.github.com>
2020-12-06 02:51:06 +0200

    Welcome page

---
+++ online-shopping/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace online_shopping.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
